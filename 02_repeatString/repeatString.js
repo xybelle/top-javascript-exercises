3547b28919606594772ac020423486580fd9bea2
xybelle <150109343+xybelle@users.noreply.github.com>
2024-06-03 16:59:35 +0800

    Fix repeatString function and remove .skip in spec

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num == 0) {
+        return '';
+    }
+    return string.repeat(num);
 };
 
 // Do not edit below this line
