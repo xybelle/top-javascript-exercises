4852463fb48c6228644f2eaef13d2d7b0e9bfc88
xybelle <150109343+xybelle@users.noreply.github.com>
2024-06-03 17:14:29 +0800

    Fix reverseString function and remove .skip in spec

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let length = word.length;
+    let reversed = [];
+    for (let i = 0; i < length; i++) {
+        reversed[i] = word[(length - 1) - i];
+    }
+    return reversed.join('');
 };
 
 // Do not edit below this line
