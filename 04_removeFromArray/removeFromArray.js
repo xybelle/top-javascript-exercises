62bf021ffde3e071e57a144d87379938ee382328
xybelle <150109343+xybelle@users.noreply.github.com>
2024-06-04 19:47:46 +0800

    Fix removeFromArray function and remove .spec in spec

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...theRestForRemoval) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (theRestForRemoval.includes(array[i])) {
+            continue;
+        }
+        newArray.push(array[i]);
+    }
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
