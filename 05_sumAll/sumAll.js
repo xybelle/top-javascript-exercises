ccacac1a2fab44402d441bfd0f82d469708cd097
xybelle <150109343+xybelle@users.noreply.github.com>
2024-06-05 16:56:02 +0800

    Fix sumAll function that takes two pos ints and returns sum inclusively

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            finalSum += i;
+        }
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            finalSum += i;
+        }
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
